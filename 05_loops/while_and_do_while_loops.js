ecbaf8516fd465e9feb1f0e409ac5b5c86da842f
pratik1335 <pratikgupta1335@gmail.com>
2025-03-30 19:12:25 +0000

    while and do-while loop

---
+++ 05_loops/while_and_do_while_loops.js
@@ -0,0 +1,32 @@
+// while loop
+
+// let index = 0;
+
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index = index + 2
+// }
+
+let myArray = ["flash", "superman", "batman"]
+
+let index = 0;
+
+while(index < myArray.length){
+    // const element = myArray[index]
+    // console.log(element);
+    console.log(`Value is ${myArray[index]}`);
+    index++
+}
+
+
+
+// do-while loop
+
+let score = 1
+
+// let score = 11
+
+do{
+    console.log(`Score is ${score}`);
+    score++
+}while(score <= 10)
